4425152566ac8ce975d97ba40340b05a0ec38eb2
Mcdouglasx <152122257+Mcdouglasx@users.noreply.github.com>
2023-11-27 16:34:27 -0400

    Add files via upload

---
+++ multy_binary_scan.py
@@ -0,0 +1,116 @@
+#@mcdouglasx
+import secp256k1 as ice
+import random
+from bitstring import BitArray
+
+
+
+print("Scanning Binary Sequence")
+
+#Pk: 1033162084
+#cPub: 030d282cf2ff536d2c42f105d0b8588821a915dc3f9a05bd98bb23af67a2e92a5b
+
+#range
+start= 1
+end=   5000000000
+
+#total number of keys in database.
+Db_items= 32000000
+
+#number of targets.
+Db_target=10
+
+X= Db_items//Db_target    
+
+while True:
+
+    pk= random.randint(start, end)
+
+    target = ice.scalar_multiplication(pk)
+
+    num = 64 # collision margin.
+
+    sustract= 1 #amount to subtract each time.
+
+    sustract_pub= ice.scalar_multiplication(sustract)
+
+    res= ice.point_loop_subtraction(num, target, sustract_pub)
+    
+    binary = ''
+    
+    for t in range (num):
+        
+        h= (res[t*65:t*65+65]).hex()
+        hc= int(h[2:], 16)
+        
+        
+        if str(hc).endswith(('0','2','4','6','8')):
+            A="0"
+            binary+= ''.join(str(A))
+            
+        if str(hc).endswith(('1','3','5','7','9')):
+            A="1"
+            binary+= ''.join(str(A))
+    
+        
+    my_str = binary
+
+    b = bytes(BitArray(bin=my_str))
+
+    file = open("data-base.bin", "rb")
+
+    dat = bytes(file.read())
+    
+    if b  in dat:
+        with open (r"data-base.bin", "rb") as file:
+            s = b
+            f = bytes(file.read())
+            inx = (f.find(s) % X)*sustract
+            Pk = (int(pk) + int(inx))+int(inx)*7
+        
+            data = open("win.txt","a")
+            data.write("Pk:"+" "+str(Pk)+"\n")
+            data.close()
+            break
+    for t in range (num):
+        
+        h= (res[t*65:t*65+65]).hex()
+        hc= int(h[2:], 16)
+        
+        
+        if str(hc).endswith(('0','2','4','6','8')):
+            A="0"
+            binary+= ''.join(str(A))
+            
+        if str(hc).endswith(('1','3','5','7','9')):
+            A="1"
+            binary+= ''.join(str(A))
+    
+        
+    my_str = binary
+
+    b = bytes(BitArray(bin=my_str))
+
+    file = open("data-base.bin", "rb")
+
+    dat = bytes(file.read())
+    
+    if b  in dat:
+        with open (r"data-base.bin", "rb") as file:
+            s = b
+            f = bytes(file.read())
+            inx = f.find(s)% N
+            Pk = (int(pk) + int(inx))+int(inx)*7
+        
+            data = open("win.txt","a")
+            data.write("Pk:"+" "+str(Pk)+"\n")
+            data.close()
+            break
+        
+        
+
+   
+
+    
+
+    

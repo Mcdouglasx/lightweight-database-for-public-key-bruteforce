8f6dfff3d479eb3a7d29c3f65f17c746d902ce25
Mcdouglasx <152122257+Mcdouglasx@users.noreply.github.com>
2023-11-28 19:38:35 -0400

    Update multy_binary_scan.py

--- multy_binary_scan.py
+++ multy_binary_scan.py
@@ -11,16 +11,17 @@ print("Scanning Binary Sequence")
 #cPub: 030d282cf2ff536d2c42f105d0b8588821a915dc3f9a05bd98bb23af67a2e92a5b
 
 #range
-start= 1
-end=   5000000000
+start= 1033000000
+end=   1034000000
 
 #total number of keys in database.
-Db_items= 32000000
+Db_items= 160000
 
 #number of targets.
-Db_target=10
-
-X= Db_items//Db_target    
+Db_target=16
+A_x= Db_items//Db_target 
+X= A_x
+print(X)
 
 while True:
 
@@ -65,52 +66,14 @@ while True:
         with open (r"data-base.bin", "rb") as file:
             s = b
             f = bytes(file.read())
-            inx = (f.find(s) % X)*sustract
-            Pk = (int(pk) + int(inx))+int(inx)*7
+            inx = (f.find(s) % X)
+            inx_0= inx*sustract
+            Pk = (int(pk) + int(inx_0))+int(inx_0)*7
+            pub= ice.scalar_multiplication(Pk).hex()
+            cpub= ice.to_cpub(pub)
         
             data = open("win.txt","a")
             data.write("Pk:"+" "+str(Pk)+"\n")
+            data.write("cpub:"+" "+str(cpub)+"\n")
             data.close()
             break
-    for t in range (num):
-        
-        h= (res[t*65:t*65+65]).hex()
-        hc= int(h[2:], 16)
-        
-        
-        if str(hc).endswith(('0','2','4','6','8')):
-            A="0"
-            binary+= ''.join(str(A))
-            
-        if str(hc).endswith(('1','3','5','7','9')):
-            A="1"
-            binary+= ''.join(str(A))
-    
-        
-    my_str = binary
-
-    b = bytes(BitArray(bin=my_str))
-
-    file = open("data-base.bin", "rb")
-
-    dat = bytes(file.read())
-    
-    if b  in dat:
-        with open (r"data-base.bin", "rb") as file:
-            s = b
-            f = bytes(file.read())
-            inx = f.find(s)% N
-            Pk = (int(pk) + int(inx))+int(inx)*7
-        
-            data = open("win.txt","a")
-            data.write("Pk:"+" "+str(Pk)+"\n")
-            data.close()
-            break
-        
-        
-
-   
-
-    
-
-    

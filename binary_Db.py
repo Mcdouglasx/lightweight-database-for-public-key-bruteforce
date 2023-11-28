a80694fe31e770ecb3079bff0aac9395750827dc
Mcdouglasx <152122257+Mcdouglasx@users.noreply.github.com>
2023-11-28 19:29:49 -0400

    Update binary_Db.py

--- binary_Db.py
+++ binary_Db.py
@@ -1,5 +1,7 @@
-import secp256k1 as ice
 #@mcdouglasx
+import secp256k1 as ice
+from bitstring import BitArray
+
 print("Making Binary Data-Base")
 
 
@@ -7,29 +9,33 @@ target_public_key = "030d282cf2ff536d2c42f105d0b8588821a915dc3f9a05bd98bb23af67a
 
 target = ice.pub2upub(target_public_key)
 
-num = 32000000 # number of times.
+num = 1000 # number of keys.
 
 sustract= 1 #amount to subtract each time.
 
 sustract_pub= ice.scalar_multiplication(sustract)
 
 res= ice.point_loop_subtraction(num, target, sustract_pub)
+binary = ''
+for t in range (num):
 
-for t in range (num+1):
     h= (res[t*65:t*65+65]).hex()
     hc= int(h[2:], 16)
-    
-    
+        
+        
     if str(hc).endswith(('0','2','4','6','8')):
         A="0"
-        data = open("Dat-Bin.txt","a")
-        data.write(A)
-        data.close()
-        
+        binary+= ''.join(str(A))
+            
     if str(hc).endswith(('1','3','5','7','9')):
         A="1"
-        data = open("Dat-Bin.txt","a")
-        data.write(A)
-        data.close()
+        binary+= ''.join(str(A))
+        
+
+my_str = bytes(BitArray(bin=binary))
+
+binary_file = open('data-base.bin', 'wb')
+binary_file.write(my_str)
+binary_file.close()
 
     
